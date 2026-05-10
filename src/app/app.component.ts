33078fee0cf3ed0c3044f8f6eb29623610edbf0f
itagiba <itagiba.net@gmail.com>
2026-05-09 22:22:09 -0300

    feat: update welcome component with detailed content and routing setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { WelcomeComponent } from './welcome/welcome.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [WelcomeComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { CalculadoraComponent } from './calculadora/calculadora.component';
+import { ListaComprasComponent } from './lista-compras/lista-compras.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: WelcomeComponent },
+  { path: 'calculadora', component: CalculadoraComponent },
+  { path: 'lista-compras', component: ListaComprasComponent },
+];
