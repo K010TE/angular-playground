a0e188a802262fb76d83fc7a288ddd7418c310a9
itagiba <itagiba.net@gmail.com>
2026-05-09 22:07:04 -0300

    feat: implement shopping list component with item management

---
+++ src/app/lista-compras/lista-compras.component.ts
@@ -0,0 +1,43 @@
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ItemList } from './itemList';
+
+@Component({
+  selector: 'app-lista-compras',
+  imports: [FormsModule],
+  templateUrl: './lista-compras.component.html',
+  styleUrl: './lista-compras.component.scss'
+})
+export class ListaComprasComponent {
+  item = signal<string>('');
+  list = signal<ItemList[]>([]);
+
+  addItem() {
+    
+    let itemList = new ItemList();
+    itemList.name = this.item();
+    itemList.id = this.list().length + 1;
+    this.list.update(items => [...items, itemList]);
+    this.item.set('');
+
+  }
+
+  checkItem(item: ItemList) {
+    item.checked = !item.checked;
+    this.list.update(items => [...items]);
+  }
+
+
+  deleteItem(item: ItemList) {
+    if (confirm(`Tem certeza que deseja remover o item ${item.name}?`)) {
+      this.list.update(items => items.filter(i => i.id !== item.id));
+    }
+  }
+
+  deleteList() {
+    if (confirm('Tem certeza que deseja limpar toda a lista?')) {
+      this.list.set([]);
+    }
+  }
+
+}
